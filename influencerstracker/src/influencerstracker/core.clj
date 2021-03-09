f54478edb92fcc700446af23fc922c2dc61d65eb
nemanja.radulovic <nemanja.radulovic@NRADULOVIC-NB>
2021-03-09 20:36:52 +0100

    template for web app with hiccup

--- influencerstracker/src/influencerstracker/core.clj
+++ influencerstracker/src/influencerstracker/core.clj
@@ -1,14 +1,20 @@
 (ns influencerstracker.core
   (:require [clojure.java.jdbc :as sql]
-            [clj-time.core :as t]
-            [clj-time.coerce :as c]
-            [clj-time.local :as l]
             [yesql.core :refer [defquery defqueries]]
-            [twitchapi :refer :all]))
+            [influencerstracker.twitchapi :refer :all]
+            [compojure.core :refer [defroutes GET ANY]]
+            [compojure.route :as route]
+            [compojure.handler :as handler]
+            [ring.adapter.jetty :as jetty]
+            [influencerstracker.views.layout :as layout]
+            [influencerstracker.views.contents :as contents]))
 
 (def database {:subprotocol "postgresql"
          :subname "//localhost/influencers"
          :user "postgres"})
+
+(defn parse-int [s]
+  (Integer. (re-find  #"\d+" s)))
 
 ; id SERIAL-autoincrement :name, :game, :views, :language, :timestamp
 (defn create-influencers-table! []
@@ -16,49 +22,68 @@
                       (sql/create-table-ddl
                        :influencers
                        [[:id "SERIAL"]
-                        [:user_id "varchar(200)"]
+                        [:user_id "integer"]
                         [:username "varchar(200)"]
                         [:game "varchar(200)"]
-                        [:views "varchar(200)"]
+                        [:views "integer"]
                         [:language "varchar(200)"]
                         [:timestamp "TIMESTAMP"]])))
 
 (defqueries "influencers.sql" {:connection database})
 
-(find-influencers)
-
-(create-influencer {:name "bar" :game "wow" :views "1000" :language "en" :timestamp (str  (new java.util.Date))})
-
-(influencers-count)
-
-(influencer-by-id {:id 2})
-
-(create-influencer 
- {:user_id (twitchapi/get-streams-top-id)
-  :username (twitchapi/get-streams-top-name) 
-  :game (twitchapi/get-streams-top-gamename) 
-  :views (twitchapi/get-stream-viewcount) 
-  :language (twitchapi/get-stream-language)})
-
-(influencer-order-by-timestamp)
-
-(influencer-last-7-days)
-
-(change-timestamp {:id 2})
-
+;; Add influencer to database
+(create-influencer!
+          {:user_id (parse-int (influencerstracker.twitchapi/get-streams-top-id))
+           :username (influencerstracker.twitchapi/get-streams-top-name)
+           :game (influencerstracker.twitchapi/get-streams-top-gamename)
+           :views (influencerstracker.twitchapi/get-stream-viewcount)
+           :language (influencerstracker.twitchapi/get-stream-language)})
 
 ;; loop in background collecting data from twitch api
 (defn set-interval [callback ms]
   (future (while true (do (Thread/sleep ms) (callback)))))
 
-;; start work in background
+;; start work in background every 15min (900000 miliseconds) collect data
 (def job (set-interval 
-         (create-influencer
-          {:user_id (twitchapi/get-streams-top-id)
-           :username (twitchapi/get-streams-top-name)
-           :game (twitchapi/get-streams-top-gamename)
-           :views (twitchapi/get-stream-viewcount)
-           :language (twitchapi/get-stream-language)}) 900000))
+         #(create-influencer!
+          {:user_id (parse-int (influencerstracker.twitchapi/get-streams-top-id))
+           :username (influencerstracker.twitchapi/get-streams-top-name)
+           :game (influencerstracker.twitchapi/get-streams-top-gamename)
+           :views (influencerstracker.twitchapi/get-stream-viewcount)
+           :language (influencerstracker.twitchapi/get-stream-language)}) 900000))
 
 ;; cancel background job
 (future-cancel job)
+
+
+
+(find-influencers)
+
+(influencers-count)
+
+(influencer-by-id {:id 252})
+
+(influencer-order-by-timestamp)
+
+(influencer-last-7-days)
+
+(influencer-with-biggest-live-views)
+
+(game-rank-1)
+
+
+(defroutes routes
+  (GET "/" [] (layout/application "Home" (contents/index)))
+  (GET "/hello" [] (layout/application "Hello ???" (contents/hello)))
+  (GET "/subscribe" [] (layout/application "Subscrition" (contents/subscribe)))
+  (GET "/pagination" [] (layout/application "Pagination" (contents/pagination)))
+  (GET "/pages/:id" [id]  (contents/page id))
+  (route/resources "/")
+  (ANY "*" [] (route/not-found (layout/application "Page Not Found" (contents/not-found)))))
+
+(def application (handler/site routes))
+
+(defn -main []
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
+    (jetty/run-jetty application {:port port :join? false})))
+
---
+++ influencerstracker/src/influencerstracker/views/contents.clj
@@ -0,0 +1,59 @@
+(ns influencerstracker.views.contents
+  (:use [hiccup.form]
+        [hiccup.element :only (link-to)]
+        [influencerstracker.twitchapi :refer :all]
+        [yesql.core :refer [defquery defqueries]]))
+
+(defn index []
+  [:div {:id "content"}
+   [:h1 {:class "text-success"} "Hello Hiccup " (influencerstracker.twitchapi/get-streams-top-name) ]
+   [:h2 (find-influencers)]])
+
+(defn hello []
+  [:div {:class "well"}
+   [:h1 {:class "text-info"} "Hello Hiccup and AngularJS"]
+   [:div {:class "row"}
+    [:div {:class "col-lg-2"}
+     (label "name" "Name:")]
+    [:div {:class "col-lg-4"}
+     (text-field {:class "form-control" :ng-model "yourName" :placeholder "Enter a name here"} "your-name")]]
+   [:hr]
+   [:h1 {:class "text-success"} "Hello {{yourName}}!"]])
+
+(defn labeled-radio [label]
+  [:label (radio-button {:ng-model "user.gender"} "user.gender" false label)
+   (str label "    ")])
+
+(defn subscribe []
+  [:div {:class "well"}
+   [:form {:novalidate "" :role "form"}
+    [:div {:class "form-group"}
+     (label {:class "control-label"} "email" "Email")
+     (email-field {:class "form-control" :placeholder "Email" :ng-model "user.email"} "user.email")]
+    [:div {:class "form-group"}
+     (label {:class "control-label"} "password" "Password")
+     (password-field {:class "form-control" :placeholder "Password" :ng-model "user.password"} "user.password")]
+    [:div {:class "form-group"}
+     (label {:class "control-label"} "gender" "Gender")
+     (reduce conj [:div {:class "btn-group"}] (map labeled-radio ["male" "female" "other"]))]
+    [:div {:class "form-group"}
+     [:label
+      (check-box {:ng-model "user.remember"} "user.remember-me") " Remember me"]]]
+   [:pre "form = {{ user | json }}"]])
+
+(defn pagination []
+  [:div {:ng-controller "PaginationCtrl" :class "well"}
+   [:pre "[Browser] Current page: {{currentPage}}. [Server] {{partial}}"]
+   [:pagination {:total-items "totalItems" :page "currentPage" :on-select-page "displayPartial(page)"}]])
+
+(defn parse-integer [s]
+  (Integer. (re-find  #"\d+" s)))
+
+(defn page [id]
+  (influencer-by-id {:id (parse-integer id)}))
+
+(defn not-found []
+  [:div {:class "well"}
+   [:h1 {:class "info-worning"} "Page Not Found"]
+   [:p "There's no requested page. "]
+   (link-to {:class "btn btn-primary"} "/" "Take me to Home")])
