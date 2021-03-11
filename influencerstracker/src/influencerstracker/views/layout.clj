a4c421109b3ff3dc096e9892fe9a9fa07b1577fa
nemanja.radulovic <nemanja.radulovic@NRADULOVIC-NB>
2021-03-11 19:53:05 +0100

    minor fixes. moved queries to db, added index.clj

--- influencerstracker/src/influencerstracker/views/layout.clj
+++ influencerstracker/src/influencerstracker/views/layout.clj
@@ -1,14 +1,24 @@
   (ns influencerstracker.views.layout
   (:use [hiccup.page :only (html5 include-css include-js)]))
 
+(defn- navigation-top
+  []
+    [:nav {:class "navbar navbar-light bg-light"}
+     [:a {:class "navbar-brand" :href "#"}
+      [:img {:src "/docs/4.0/assets/brand/bootstrap-solid.svg" :width "30" :height "30" :alt ""}]]
+     [:a {:class "navbar-brand" :href "#"} "Games"]
+     [:a {:class "navbar-brand" :href "#"} "Language"]])
+
 (defn application [title & content]
   (html5 {:ng-app "myApp" :lang "en"}
          [:head
           [:title title]
-          (include-css "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css")
-          (include-js "http://code.angularjs.org/1.2.3/angular.min.js")
-          (include-js "js/ui-bootstrap-tpls-0.7.0.min.js")
-          (include-js "js/script.js")
+          (include-css "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css")
+          ;; (include-js "http://code.angularjs.org/1.2.3/angular.min.js")
+          ;; (include-js "js/ui-bootstrap-tpls-0.7.0.min.js")
+          ;; (include-js "js/script.js")
 
           [:body
+           (navigation-top)
+           [:div]
            [:div {:class "container"} content]]]))
